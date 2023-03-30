0e05494bb521bfb34cdaea3dce63bba34b1d64de
Therese Hume <Therese.Hume@atu.ie>
2023-03-30 15:30:41 +0100

    Add project files.

---
+++ Match.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testfile
+{
+    internal class Match
+    {
+        public string Team1 { get; private set; }
+        public string Team2 { get; private set; }
+        public int Goals1 { get; private set; }
+        public int Goals2 { get; private set; }
+
+        public int NYellows { get; private set; }
+
+        public List<string> YellowNames { get; private set; }
+
+        public Match(string team1, string team2, int g1,int g2,int yellows, List<string> yellowNames)
+        {
+            Team1 = team1;
+            Team2 = team2;
+            Goals1 = g1;
+            Goals2 = g2;
+            NYellows= yellows;
+            YellowNames = yellowNames;
+            
+        }
+        public override string ToString() 
+        { 
+            string s= $" {Team1} : {Goals1}  {Team2} : {Goals2}  Yellow cards:";
+            foreach(string s1 in YellowNames)
+            {
+              s+= s1;
+            }
+            return s;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace testfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../results.csv";
+            List<Match> matches = new List<Match>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string input = sr.ReadLine(); 
+                Console.WriteLine("First Line is:" + input);
+
+                while ((input= sr.ReadLine())!=null) 
+                {
+
+                    string[] fields = input.Split(',');
+                    int goals1, goals2,nYellows;
+
+                    if ((fields.Length >=5) 
+                        && int.TryParse(fields[2], out goals1) 
+                        && int.TryParse(fields[3], out goals2)
+                        && int.TryParse(fields[4], out nYellows)) 
+                    {
+                        List<string> names =new List<string>();
+
+                        for(int i=5;i<fields.Length;i++)
+                            names.Add(fields[i]);
+
+                        Match m = new Match(fields[0], fields[1], goals1, goals2,nYellows,names);
+                        Console.WriteLine("Just created:" + m.ToString());
+                        matches.Add(m);
+                    
+                    }                                       
+              
+                }
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    Console.WriteLine(matches[i].ToString());
+                }
+
+
+            }
+        }
+    }
+}
